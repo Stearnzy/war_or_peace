e4b144513c1685a1c9ab9c57a3a269bb1db9d99d
Zach Stearns <zach.stearns92@gmail.com>
2020-08-19 17:11:10 -0600

    Update player test

--- test/player_test.rb
+++ test/player_test.rb
@@ -5,7 +5,7 @@ require './lib/deck'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
-def test_player_exists_and_has_name
+  def test_player_exists_and_has_name
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -29,8 +29,21 @@ def test_player_exists_and_has_name
     card3 = Card.new(:heart, 'Ace', 14)
     deck = Deck.new([card1, card2, card3])
     player = Player.new("Clarissa", deck)
-    require "pry"; binding.pry
     refute player.has_lost?
+  end
+
+  def test_player_loses_when_out_of_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new("Clarissa", deck)
+    player.deck.remove_card
+    player.deck.remove_card
+    refute player.has_lost?
+    player.deck.remove_card
+    assert_equal [], player.deck.cards
+    assert player.has_lost?
   end
 end
 
