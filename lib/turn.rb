7a1a0ac0062b1f2f2bf69c0e005853b7d0557430
Zach Stearns <zach.stearns92@gmail.com>
2020-08-19 17:20:07 -0600

    Update turn file and test

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+class Turn
+  attr_reader :player1, :player2, :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  # def type
+  #   if player1.deck.card.rank_of_card_at(0) != player2.deck.card.rank_of_card_at(0)
+  #     return :basic
+  #   elsif player1.deck.card.rank_of_card_at(0) == player2.deck.card.rank_of_card_at(0)
+  #     return :war
+  #   elsif player1.deck.card.rank_of_card_at(0) && player1.deck.card.rank_of_card_at(2) == player2.deck.card.rank_of_card_at(0) && player2.deck.card.rank_of_card_at(2)
+  #     return :mutually_assured_destruction
+  #   end
+  # end
+end
