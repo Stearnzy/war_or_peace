5bcc93b9677cfc5f9d7a9d6af258c216c8ad53ff
Zach Stearns <zach.stearns92@gmail.com>
2020-08-18 21:06:13 -0600

    Update files

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,25 +2,30 @@ class Deck
   attr_reader :cards
 
   def initialize(cards)
-    @cards = [card1, card2, card3]
-    @high_ranking_cards = []
+    @cards = cards
+    @high_cards = []
   end
 
   def rank_of_card_at(index)
-    return cards[index].rank
+    @cards[index].rank
   end
 
   def high_ranking_cards
-    cards.each do |card|
-      @high_ranking_cards << card if card.rank >= 11
+    @cards.each do |card|
+      @high_cards << card if card.rank >= 11
     end
+    return @high_cards
   end
 
+  def percent_high_ranking
+    @cards.each do |card|
+      @high_cards << card if card.rank >= 11
+    end
+    (@high_cards.length / 3.00 * 100).round(2)
+  end
 
+  def add_card(card)
+    @cards << card
+  end
 end
 
-card1 = Card.new(:diamond, 'Queen', 12)
-card2 = Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-
-deck = Deck.new(cards)
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,8 +5,59 @@ require './lib/card'
 
 class DeckTest < Minitest::Test
   def test_it_exists
-    deck = Deck.new
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    assert_instance_of Deck, deck
   end
 
-  def 
+  def test_deck_contains_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    assert_equal [card1, card2, card3], deck.cards
+  end
+
+  def test_can_determine_specific_card_rank
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+  end
+
+  def test_can_return_face_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    assert_equal [card1, card3], deck.high_ranking_cards
+  end
+
+  def test_can_return_high_rank_percentage_of_hand
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    assert_equal 66.67, deck.percent_high_ranking
+  end
+
+  def test_can_add_new_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+    require "pry"; binding.pry
+  end
+
 end
