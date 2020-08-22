f7d89268a8641e2fe0c464825912d654ebabd535
Zach Stearns <zach.stearns92@gmail.com>
2020-08-21 19:10:54 -0600

    Adjust mutually assured destruction details.

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,12 +9,12 @@ class Turn
   end
 
   def type
-    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-      return :basic
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       return :mutually_assured_destruction
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       return :war
+    elsif player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      return :basic
     end
   end
 
