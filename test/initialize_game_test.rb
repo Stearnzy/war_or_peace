6ce210bc08fed9b326b1920958c342fb1dd33771
Zach Stearns <zach.stearns92@gmail.com>
2020-08-22 20:21:08 -0600

    Finalize game start

--- test/initialize_game_test.rb
+++ test/initialize_game_test.rb
@@ -11,4 +11,17 @@ class InitializeGameTest < Minitest::Test
     start = InitializeGame.new
     assert_instance_of InitializeGame, start
   end
+
+  def test_ending_game_message
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+
+    deck1 = Deck.new([card1])
+    deck2 = Deck.new([card2])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    require "pry"; binding.pry
+  end
 end
