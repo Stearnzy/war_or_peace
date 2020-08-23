5dd37d6fe89e199c5a1a884fdbc91bff0c31358f
Zach Stearns <zach.stearns92@gmail.com>
2020-08-23 14:57:04 -0600

    Finalize and debug

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,19 +19,19 @@ class Turn
 
   def winner
     if self.type == :basic
-      if player1.deck.cards[0].rank > player2.deck.cards[0].rank
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
         return player1
-      elsif player2.deck.cards[0].rank < player2.deck.cards[0].rank
+      else
         return player2
       end
     elsif self.type == :war
-      if player1.deck.cards[2].rank > player2.deck.cards[2].rank
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
         return player1
-      elsif player1.deck.cards[2].rank < player2.deck.cards[2].rank
+      else
         return player2
       end
     elsif self.type == :mutually_assured_destruction
-      return "No Winner"
+      "No Winner"
     end
   end
 
